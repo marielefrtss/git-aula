c0a0d102817a2b51e426d1fd21aeb1621e8cd631
maria.mariele.2009 <maria.mariele.2009@aluno.edu.pi.gov.br>
2026-05-13 13:06:54 +0000

    criamos uma nova função para adicionar tarefas

--- todo.js
+++ todo.js
@@ -2,7 +2,7 @@ let tarefa=[];
 function adicionartarefa(novatarefa){
     lista.push(novatarefa);
     console.log("A tarefa,novatarefa",novatarefa,"foi adiciona com sucesso");
-        console.log(lista);
+    console.log(lista);
     }
 
 
