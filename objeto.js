028141d48fbf096ec57c5e7928fc23f58c8717e3
maria.mariele.2009 <maria.mariele.2009@aluno.edu.pi.gov.br>
2026-06-16 19:18:42 +0000

    objeto.js

---
+++ objeto.js
@@ -0,0 +1,19 @@
+const pessoa={
+    nome:"lorrany",
+    idade:17,
+    peso:40,
+    altura:1.60,
+    sexo:"feminino",
+}
+
+console.log (pessoa.nome);
+
+console.log (pessoa.idade);
+
+pessoa.peso=50;
+console.log (pessoa.peso);
+
+pessoa.altura=1.70;
+console.log (pessoa.altura);
+
+console.log (pessoa.sexo);
