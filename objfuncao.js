8082cc611003cb3931dcf5137a0f465a1bdf9d70
maria.mariele.2009 <maria.mariele.2009@aluno.edu.pi.gov.br>
2026-06-22 12:51:41 +0000

    objfuncao.js

--- objfuncao.js
+++ objfuncao.js
@@ -1,47 +1,25 @@
-const calculadora= {
-numero1:10,
-numero2:5,
-
-multiplicar(){
-    return this.numero1* this.numero2},
+const calculadora={
+    numero1:10,
+    numero2:5,
 
 
+    somar(){
+    return  this.numero1 + this.numero2;
+    },    
+
+    subtrair(){
+        return  this.numero1 - this.numero2;
+    },   
+
+    dividir(){
+    return  this.numero1 / this.numero2;
+    },    
+
+    multiplicar(){
+        return  this.numero1 * this.numero2;
+    },                                  
 }
-
-console.log (calculadora.multiplicar());
-
-
-const calculadoraSoma = {
-    numero1: 10,
-    numero2: 5,
-    somar() { 
-        return this.numero1 + this.numero2 
-    }
-}
-console.log(calculadoraSoma.somar());
-
-
-
-
-const calculadoraSubtrair = {
-    numero1: 10,
-    numero2: 5,
-    subtrair() { 
-        return this.numero1 - this.numero2 
-    }
-}
-console.log(calculadoraSubtrair.subtrair());
-
-
-const calculadoradividir = {
-    numero1: 10,
-    numero2: 5,
-
-    dividir() {
-        return this.numero1 / this.numero2;
-    }
-}
-
-console.log(calculadoradividir.dividir());
-
-   
+console.log(calculadora.multiplicar());
+console.log(calculadora.somar());
+console.log(calculadora.subtrair());
+console.log(calculadora.dividir());
