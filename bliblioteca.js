ca837db51edadd6b647a9ec352a079f68d04c1be
maria.mariele.2009 <maria.mariele.2009@aluno.edu.pi.gov.br>
2026-06-25 17:49:55 +0000

    bliblioteca.js

--- bliblioteca.js
+++ bliblioteca.js
@@ -19,8 +19,36 @@ const biblioteca = {
             const L= this.livros[i];
             console.log(L.nome + " - " + L.autor + " - " + L.ano + " - " + L.genero);
         }
+    },
+
+    alugar(nome){
+        for(let i=0; i<this.livros.length; i++){
+    const L= this.livros[i]
+    if(L.nome === nome){
+    if (L.disponivel==true&& L.alugado==false){
+        L.disponivel=false;
+        L.alugado=true;
+        L.vezesAlugado++;
+        L.estoque=1;
+        console.log (nome,"alugado com sucesso");
+        return;
+    }else if (L.estoque<=0){
+        console.log ("o, "+nome + "esta com estoque zerado");
+        return;
+    }else{
+    console.log ("o",+nome + "ja esta alugado")
+    return;}
     }
-};
+}
+console.log ("o",+nome + "nao exixte");
+},
+
+
+        }
+
+
+            
+
 biblioteca.adicionarLivros(
     "harry potter",
     "J.K. Rowling",
@@ -42,3 +70,5 @@ biblioteca.adicionarLivros(
 );
 console.log(biblioteca.livros);
 biblioteca.mostrarLivros();
+
+
